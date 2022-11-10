373b59d0c5bb1d966f89d35c954f328a2d1df4f1
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2022-11-10 23:55:24 +0000

    Initial commit: credit & select_word functions added

--- run.py
+++ run.py
@@ -1,3 +1,42 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from words import words
+
+def opening_credits():
+    print(
+        """
+                
+__    __                                                                 
+|  \  |  \                                                                
+| $$  | $$  ______   _______    ______   ______ ____    ______   _______  
+| $$__| $$ |      \ |       \  /      \ |      \    \  |      \ |       \ 
+| $$    $$  \$$$$$$\| $$$$$$$\|  $$$$$$\| $$$$$$\$$$$\  \$$$$$$\| $$$$$$$\
+| $$$$$$$$ /      $$| $$  | $$| $$  | $$| $$ | $$ | $$ /      $$| $$  | $$
+| $$  | $$|  $$$$$$$| $$  | $$| $$__| $$| $$ | $$ | $$|  $$$$$$$| $$  | $$
+| $$  | $$ \$$    $$| $$  | $$ \$$    $$| $$ | $$ | $$ \$$    $$| $$  | $$
+\$$   \$$  \$$$$$$$ \$$   \$$ _\$$$$$$$ \$$  \$$  \$$  \$$$$$$$ \$$   \$$
+                              |  \__| $$                                  
+                              \$$    $$                                  
+                               \$$$$$$                                   
+        
+        """
+                
+    )
+
+    print("Welcome to Hangman! Ready to play?")
+
+def select_word():
+    """
+    Generates a random word from the words.py file. The player must guest
+    this word. A new word is selected after each round.
+    """
+    random_word = random.choice(words).upper()
+    return random_word
+
+def main():
+    """
+    To run the game and present the opening credits to the user
+    """
+    opening_credits()
+    select_word()
+
+main()
