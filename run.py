45164a71a6e587c86be513c6552b847bed9f9317
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2022-11-12 14:09:05 +0000

    Add play_game function & move intro graphic

--- run.py
+++ run.py
@@ -2,7 +2,34 @@ import random
 from words import words
 
 def opening_credits():
-    print(
+    global players_name
+    
+    players_name = input("Enter your name: ")
+    print(f"Hi {players_name}, let's play!")
+
+def play_game(word):
+    """
+    To initate the start of the game for the user to begin playing
+    """
+
+
+def select_word():
+    """
+    Generates a random word from the words.py file. The player must guest
+    this word. A new word is selected after each round.
+    """
+    random_word = random.choice(words).upper()
+    return random_word
+
+def main():
+    """
+    To run the game and present the opening credits to the user
+    """
+    opening_credits()
+    select_word()
+    play_game(select_word())
+
+print(
         """
                 
 __    __                                                                 
@@ -22,21 +49,6 @@ __    __
                 
     )
 
-    print("Welcome to Hangman! Ready to play?")
-
-def select_word():
-    """
-    Generates a random word from the words.py file. The player must guest
-    this word. A new word is selected after each round.
-    """
-    random_word = random.choice(words).upper()
-    return random_word
-
-def main():
-    """
-    To run the game and present the opening credits to the user
-    """
-    opening_credits()
-    select_word()
+print("Welcome to Hangman!")
 
 main()
